b6a6f1d1ccc17f47f72efaf76587c77121d44939
Richard Hanolt <rhanolt@gmail.com>
2020-04-08 01:01:53 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,4 @@
 def greeting(name = "you")
-  puts "Please enter your name so that we can greet you more personally:"
   name = gets.strip
   puts "Hi #{name}! Welcome to the wonderful world of Ruby programming."
 end
